fccae13bd372c03c5cabac8439377b5270a5bf7f
siddheshm303 <siddheshm303@gmail.com>
2025-07-31 20:57:26 +0530

    Added variables learning

--- 02-cron-logger/log.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-echo "Cron job ran at: $(date)" >> "/mnt/d/Linux Learning/linux-learning/02-cron-logger/output.txt"
---
+++ 02-variables/variables.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#Create Variable
+name="Siddhesh"
+echo "My name is $name."
+
+#Redefining the variable
+name="Sushant"
+echo "I am $name now."
+
+#Constant Variable
+readonly college="SPDC"
+echo "My college is $college."
+
+#Try to Redefine constant variable
+college="NKT"
+echo "Now it is $college."
